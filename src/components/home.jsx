1f2401837966cb1995ba942332d4d09b4aa1dc80
Maher <maher91syr@gmail.com>
2024-11-06 17:49:48 +0900

    home page added

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page U Fuckers</h1>
+            <p></p>
+        </div>
+    );
+}
+
+export default Home;
